5a75eaa86da4a4576519d31c58481baba60c7f64
CodeNur <dodgechalanger9@gmail.com>
2025-04-03 16:27:11 +0500

    Adjust SearchSection positioning and comment out UpdateBook component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { HeaderCorusel } from "../components/HeaderCorusel";
 import Kategoriya from "../components/Kategoriya";
 import Navbar from "../components/Navbar";
 import SearchSection from "../components/SearchSection";
+// import UpdateBook from "../components/UpdateData";
 
 function Home() {
   return (
     <>
       <Navbar className="border-b border-[#292929]" />
       <HeaderCorusel />
-      <SearchSection className="relative bottom-[55px] left-[240px] flex" />
+      <SearchSection className="relative bottom-[55px] left-[230px] flex" />
       <Kategoriya />
       <BooksList />
+      {/* <UpdateBook /> */}
     </>
   );
 }
